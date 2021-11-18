e70e60ca18b7ddcd5d0bb0539f06e443126f1fc5
Lucas Kauer <lucasgkauer@gmail.com>
2021-11-18 17:52:05 -0300

    adjust migration

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+\Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
 
 class CreatePasswordResetsTable extends Migration
 {
