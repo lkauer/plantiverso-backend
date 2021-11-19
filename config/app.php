486e2525758174b1d5e8898f0bc14c3995c7624d
Lucas Kauer <lucasgkauer@gmail.com>
2021-11-19 16:57:57 -0300

    url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://plantiverso-backend-2ylj3.ondigitalocean.app'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
