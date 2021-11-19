cf9ba148d83cf78fa641602bcb9d295ef2d8d70d
Lucas Kauer <lucasgkauer@gmail.com>
2021-11-19 10:23:15 -0300

    refact

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://plantiverso-backend-2ylj3.ondigitalocean.app'),
 
     'asset_url' => env('ASSET_URL', null),
 
